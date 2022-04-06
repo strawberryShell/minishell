d93a0cdcb10957034283ba07faac733de7329f87
sehhong <shhong3317@gmail.com>
2022-04-06 21:55:40 +0900

    minishell.h 수정

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jiskim <jiskim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: sehhong <sehhong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 14:11:31 by sehhong           #+#    #+#             */
-/*   Updated: 2022/04/04 02:34:04 by jiskim           ###   ########.fr       */
+/*   Updated: 2022/04/06 20:57:11 by sehhong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,8 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <fcntl.h>
-# include <dirent.h>
-# include "readline/history.h"
-# include "readline/readline.h"
+# include <readline/history.h>
+# include <readline/readline.h>
 # include "libft.h"
 # include "structure.h"
 # include "ast.h"
